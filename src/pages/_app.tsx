6c09ea752d1378f9a2a5f8279bc9d50590970c67
Rafael Batista <rafaabatistas@gmail.com>
2021-07-25 20:11:38 -0300

    FEAT: Adicionando arquivos de config do nextjs

---
+++ src/pages/_app.tsx
@@ -0,0 +1,22 @@
+import Head from 'next/head';
+
+import type { AppProps } from 'next/app';
+
+import GlobalStyles from '~styles/global';
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Boilerplate NextJS</title>
+        <link rel="shortcut icon" href="assets/img/icon-react.png" type="image/x-icon" />
+        <link rel="apple-touch-icon" href="assets/img/icon-react.png" type="image/x-icon" />
+        <meta name="description" content="Simples projeto iniciado com Typescript, NextJS, React, Styled-Components" />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default App;
